f08852afe98869ef3eb435340e016e98dc162fb8
Gunaprathap <gunasekarangp1978@gmail.com>
2025-02-21 22:54:55 +0530

    File created

---
+++ new_calc/main.js
@@ -0,0 +1,9 @@
+//select all tag
+
+let inp =document.getElementById(".input_num").value
+// inp = Number(inp)
+let result = document.getElementById("res")
+
+function results(){
+    console.log(inp)
+}
